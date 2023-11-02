71422012af6c1c9759009d9dc937baaf8d72ff47
prabhavith <you@example.com>
2023-11-02 12:45:23 +0530

    frontend.sh is added to main

--- frontend.sh
+++ frontend.sh
@@ -2,26 +2,26 @@
 
 touch frontend.log
 
-ls /usr/share/nginx/html/
-rm -rf /usr/share/nginx/html/*
-ls /usr/share/nginx/html/
+ls /usr/share/nginx/html/ >> frontend.log
+rm -rf /usr/share/nginx/html/* >> frontend.log
+ls /usr/share/nginx/html/ >> frontend.log
 
-rm -rf /tmp/*
-ls /tmp
+rm -rf /tmp/* >> frontend.log
+ls /tmp >> frontend.log
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-ls /tmp
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> frontend.log
+ls /tmp >> frontend.log
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html >> frontend.log
+unzip /tmp/frontend.zip >> frontend.log
 echo "file download status $?"
 
-cd /etc/nginx/default.d
-rm -f /etc/nginx/default.d/expense.conf
-touch expense.conf
-echo 'proxy_http_version 1.1;' >> expense.conf
-echo 'location /api/ { proxy_pass http://localhost:8080/; }' >> expense.conf
+cd /etc/nginx/default.d >> frontend.log
+rm -f /etc/nginx/default.d/expense.conf >> frontend.log
+touch expense.conf >> frontend.log
+echo 'proxy_http_version 1.1;' >> expense.conf >> frontend.log
+echo 'location /api/ { proxy_pass http://localhost:8080/; }' >> expense.conf >> frontend.log
 echo "expense.conf add status $?"
 
-systemctl restart nginx
+systemctl restart nginx >> frontend.log
 echo "nginx restart status $?"
