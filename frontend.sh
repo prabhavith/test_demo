6b17eb25a7322a94d3afb432de365e7dfe4ce774
prabhavith <you@example.com>
2023-11-02 12:42:01 +0530

    frontend.sh is added to main

--- frontend.sh
+++ frontend.sh
@@ -9,14 +9,14 @@ ls /usr/share/nginx/html/
 rm -rf /tmp/*
 ls /tmp
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &
-ls /tmp &
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+ls /tmp
 
-cd /usr/share/nginx/html &
-unzip /tmp/frontend.zip &
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 echo "file download status $?"
 
-cd /etc/nginx/default.d &
+cd /etc/nginx/default.d
 rm -f /etc/nginx/default.d/expense.conf
 touch expense.conf
 echo 'proxy_http_version 1.1;' >> expense.conf
